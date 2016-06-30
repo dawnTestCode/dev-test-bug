87dfb74a2db23347e0ed3635035744f1da8db63d
Shannon Code <genecyber@gmail.com>
2016-06-29 22:43:16 -0400

    initial

---
+++ scripts/code.js
@@ -0,0 +1,31 @@
+
+var $table = $('#table'),
+    $button = $('#button')
+
+$(function () {
+    //Make table
+    $('#table').bootstrapTable({
+        data: data
+    })
+    //Make log
+    $('#log').bootstrapTable({
+        data: log
+    })
+    //Attach click event
+    $button.click(function () {
+
+        //Collect checked row
+        var rowToRemove = $table.bootstrapTable('getSelections')[0]     
+
+        //Add to log
+        $("#log").bootstrapTable('insertRow', {index: log.length, row: data[0]})
+
+        //Remove checked rows
+        $table.bootstrapTable('remove', {
+            field: 'id',
+            values: [rowToRemove.id]
+        })
+
+        
+    })
+})
---
+++ scripts/data.js
@@ -0,0 +1,29 @@
+var log = []
+var data =  [
+        {
+            "id" : 1,
+            "name": "Sweet Baby Jesus",
+            "style": "Porter",
+            "rating": 5
+        },{
+            "id" : 2,
+            "name": "Young's Double Chocolate",
+            "style": "Stout",            
+            "rating": 4
+        },{
+            "id" : 3,
+            "name": "Sweet Josie",
+            "style": "Brown Ale",
+            "rating": 3
+        },{
+            "id" : 4,
+            "name": "Quick Start My Heart",
+            "style": "Milk Stout",
+            "rating": 4
+        },{
+            "id" : 5,
+            "name": "Bud Light",
+            "style": "Lager",
+            "rating": 1
+        }
+    ]
